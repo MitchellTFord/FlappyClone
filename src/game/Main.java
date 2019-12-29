bc4c29396ddc78cdcb05b3c9fb95bb9904575ea3
MitchellTFord <mitchelltford@gmail.com>
2019-12-29 16:29:29 -0600

    Created repository and master branch, configured gitignore

---
+++ src/game/Main.java
@@ -0,0 +1,17 @@
+package game;
+
+/**
+ * 
+ * @author Mitchell Ford
+ * @author Andy Zhang
+ */
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
